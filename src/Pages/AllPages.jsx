f3e5b0ba2d2b29310ebd9f16a50064a1a3a2e251
saidfathy <fathy50515253@gmail.com>
2023-01-11 08:28:31 +0200

    create footer-box

--- src/Pages/AllPages.jsx
+++ src/Pages/AllPages.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "../components";
+import { FooterOverlay, Header } from "../components";
 import { Home } from "./index";
 const AllPages = () => {
     return (
@@ -11,6 +11,7 @@ const AllPages = () => {
                     <Routes>
                         <Route path="/" element={<Home />} />
                     </Routes>
+                    <FooterOverlay />
                 </BrowserRouter>
             </div>
         </>
--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,11 +1,146 @@
-import React from 'react';
+import React from "react";
 
-import './FooterOverlay.css';
-
+import "./FooterOverlay.css";
+import LogoImg from "../../assets/Img/اطلبها-لوجو@2x.png";
+import { ReactComponent as Iconsnapchat } from "../../assets/Icons/snapchat.svg";
+import { ReactComponent as Iconfacebook } from "../../assets/Icons/icon-24-facebook.svg";
+import { ReactComponent as Icontwitter } from "../../assets/Icons/icon-24-twitter.svg";
+import { ReactComponent as Iconinstagram } from "../../assets/Icons/icon-32-instagram.svg";
+import { ReactComponent as Icontiktok } from "../../assets/Icons/icon-32-tiktok.svg";
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
-  </div>
+    <>
+        <footer className="main-footer ">
+            <div className="all">
+                <div className="container flex-column flex-lg-row-reverse  align-items-center align-items-lg-start ">
+                    <div className="box-left">
+                        <a className="main-footer__logo" href="#0">
+                            <img src={LogoImg} alt="" />
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <Iconfacebook />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <Iconinstagram />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <Icontwitter />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <Icontiktok />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <nav className="box-right">
+                        <ul className="">
+                            <li className=" text-center text-lg-end ">
+                                <h4 className="me6-margin-bottom-sm me6-text-base@md">
+                                    خدماتنا
+                                </h4>
+                                <ul className="text-center text-lg-end ">
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            الاستيراد
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            الأكاديمية
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            الاستشارات
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="text-center text-lg-end ">
+                                <h4 className="">معلومات</h4>
+                                <ul className="text-center text-lg-end ">
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            من نحن
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            الأسئلة الشائعة
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            سياسة الخصوصية
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="text-center text-lg-end ">
+                                <h4 className="">حسابي</h4>
+                                <ul className="text-center text-lg-end ">
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            التسجيل كتاجر
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            التسجيل كمندوب
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="text-center text-lg-end ">
+                                <h4 className="">خدمة العملاء</h4>
+                                <ul className="text-center text-lg-end ">
+                                    <li>
+                                        <a
+                                            href="#0"
+                                            className="main-footer-link"
+                                        >
+                                            اتصل بنا
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </footer>
+    </>
 );
 
 export default FooterOverlay;
